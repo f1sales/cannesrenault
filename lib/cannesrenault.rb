651acb84f0b6be71366df71088290e39dfbc9a60
Thiago Youssef <thiagodyoussef@gmail.com>
2021-11-30 16:29:30 -0300

    Add `autoforce` hook

--- lib/cannesrenault.rb
+++ lib/cannesrenault.rb
@@ -11,12 +11,21 @@ module Cannesrenault
 
     def self.switch_source(lead)
       source_name = lead.source.name
+
       if source_name.downcase.include?('facebook')
         message = lead.message.split(':').map(&:strip)
         store_question = message.index('por_qual_concessionária_você_quer_ser_atendido(a)?')
         return source_name unless store_question
 
         store_name = message[store_question + 1]
+        "#{source_name} - #{store_name}"
+      elsif source_name.downcase.include?('autoforce')
+        description = lead.description.gsub(':', '.').split('.').map(&:strip)
+        store_index = description.index('Unidade')
+        return source_name unless store_index
+
+        store_name = description[store_index + 1]
+
         "#{source_name} - #{store_name}"
       else
         source_name
